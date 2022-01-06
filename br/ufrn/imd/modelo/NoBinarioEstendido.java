b0f830295fbb78a72fa9434130a5108135d69872
Lenival <lenival@ufrn.edu.br>
2022-01-05 23:25:48 -0300

    Implementação
    Implementação inicial do NoBinarioExtendido

--- br/ufrn/imd/modelo/NoBinarioEstendido.java
+++ br/ufrn/imd/modelo/NoBinarioEstendido.java
@@ -1,5 +1,39 @@
 package br.ufrn.imd.modelo;
 
 public class NoBinarioEstendido <T extends Comparable <T>> extends NoBinario<T> {
+	private Integer qNosEsq;
+	private Integer qNosDir;
+	
+	public NoBinarioEstendido() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoBinarioEstendido(T dado) {
+		super(dado);
+		this.qNosEsq = 0;
+		this.qNosDir = 0;		
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoBinarioEstendido(T dado, Integer qNosEsq, Integer qNosDir) {
+		super(dado);
+		this.qNosEsq = qNosEsq;
+		this.qNosDir = qNosDir;
+	}
+	
+	public Integer getqNosEsq() {
+		return qNosEsq;
+	}
+	public void setqNosEsq(Integer qNosEsq) {
+		this.qNosEsq = qNosEsq;
+	}
+	public Integer getqNosDir() {
+		return qNosDir;
+	}
+	public void setqNosDir(Integer qNosDir) {
+		this.qNosDir = qNosDir;
+	}
+	
 	
 }
